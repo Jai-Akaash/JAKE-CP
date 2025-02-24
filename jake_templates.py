4560f331acd39036c805ff23fc05232766ca4e00
Jai-Akaash <152298518+Jai-Akaash@users.noreply.github.com>
2025-02-24 21:17:06 +0530

    Update dSu.py

--- jake_templates.py
+++ jake_templates.py
@@ -2,11 +2,23 @@ class DisJointSet :
 	def __init__(self,n):
 		self.rank =  [0 for _ in range(n + 1)]
 		self.parent = [i for i in range(n + 1)]
+		self.size = [1 for _ in range(n + 1)]
 	def FindParent(self,Node):
 		if self.parent[Node] == Node:
 			return Node
 		self.parent[Node] = self.FindParent(self.parent[Node])
 		return self.parent[Node]
+	def unionBySize(self,u,v):
+		Upu = self.FindParent(u)
+		Upv = self.FindParent(v)
+		if Upv == Upu:
+			return
+		if self.size[Upu] < self.size[Upv]:
+			self.parent[Upu] = Upv
+			self.size[Upv] += self.size[Upu]
+		else:
+			self.parent[Upv] = Upu
+			self.size[Upu] += self.size[Upv]
 	def unionByRank(self,u,v):
 		Upu = self.FindParent(u)
 		Upv = self.FindParent(v)
@@ -17,7 +29,7 @@ class DisJointSet :
 		else:
 			self.parent[Upv] = Upu
 			self.rank[Upu] += 1
-g = DisJointSet(5)
-g.unionByRank(1,2)
-g.unionByRank(3,4)
-print(g.FindParent(1),g.FindParent(2),g.FindParent(5))
+# g = DisJointSet(5)
+# g.unionByRank(1,2)
+# g.unionByRank(3,4)
+# print(g.FindParent(1),g.FindParent(2),g.FindParent(5))
